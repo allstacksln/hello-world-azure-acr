f14ffd85dc66430d4a0c3815d34db0595756cf41
Sourav Basu <134071309+allstacksln@users.noreply.github.com>
2023-06-20 16:14:08 -0400

    Reverted main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,4 +3,10 @@ locals {
 }
 
 # Create a virtual network within the resource group
-resource "null_resource" "example" {}
+#resource "null_resource" "example" {}
+resource "azurerm_virtual_network" "vpc" {
+  name                = "${local.unique_name}-network"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  address_space       = ["10.1.0.0/16"]
+}
